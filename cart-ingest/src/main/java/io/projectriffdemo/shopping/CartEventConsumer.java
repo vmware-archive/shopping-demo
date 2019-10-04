eb38bafdedf45dd20e6e4eeb226873ba752cbb71
Florent Biville <fbiville@pivotal.io>
2019-10-04 22:17:12 +0200

    Make cart event ingestion configurable

---
+++ cart-ingest/src/main/java/io/projectriffdemo/shopping/CartEventConsumer.java
@@ -0,0 +1,31 @@
+package io.projectriffdemo.shopping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.function.Consumer;
+
+public class CartEventConsumer implements Consumer<CartEvent> {
+
+
+    private final RestTemplate restTemplate;
+    private final String streamUrl;
+
+    @Autowired
+    public CartEventConsumer(RestTemplate restTemplate,
+                             @Value("${streaming.gateway}") String streamingGateway,
+                             @Value("${streaming.stream.namespace:default}") String streamNamespace,
+                             @Value("${streaming.stream.name}") String streamName) {
+
+        this.restTemplate = restTemplate;
+        this.streamUrl = String.format("http://%s.riff-system.svc.cluster.local/%s/%s", streamingGateway, streamNamespace, streamName);
+    }
+
+    @Override
+    public void accept(CartEvent cartEvent) {
+        System.out.println("Publishing event:" + cartEvent);
+        restTemplate.postForLocation(streamUrl, cartEvent);
+        System.out.println("Published");
+    }
+}
--- cart-ingest/src/main/java/io/projectriffdemo/shopping/CartIngestApplication.java
+++ cart-ingest/src/main/java/io/projectriffdemo/shopping/CartIngestApplication.java
@@ -2,29 +2,11 @@ package io.projectriffdemo.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-
-import java.util.function.Consumer;
 
 @SpringBootApplication
 public class CartIngestApplication {
 
-    public final String STREAMING_HTTP_GATEWAY_NAME = "riff-streaming-http-gateway";
-    public final String STREAM_NAME = "cart-events";
-
     public static void main(String[] args) {
         SpringApplication.run(CartIngestApplication.class, args);
-    }
-
-    @Bean
-    public Consumer<CartEvent> consumeCartEvent() {
-        return e -> {
-            System.out.println("Consuming event"+ e);
-            RestTemplate restTemplate = new RestTemplate();
-            restTemplate.postForLocation("http://"+STREAMING_HTTP_GATEWAY_NAME+".riff-system.svc.cluster.local/default/"+STREAM_NAME, e);
-            System.out.println("published");
-        };
     }
 }
