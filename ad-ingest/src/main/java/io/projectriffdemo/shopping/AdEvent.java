e8aa1124d1fd8e79760242cf4e945f22fea2fc47
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-10-07 06:01:36 -0700

    adding fields to AdEvent

--- ad-ingest/src/main/java/io/projectriffdemo/shopping/AdEvent.java
+++ ad-ingest/src/main/java/io/projectriffdemo/shopping/AdEvent.java
@@ -3,7 +3,19 @@ package io.projectriffdemo.shopping;
 public class AdEvent {
 
     private String itemId;
+    private String action;
+    private String relatedTo;
     private String message;
+
+    public AdEvent() {
+    }
+
+    public AdEvent(String itemId, String action, String relatedTo, String message) {
+        this.itemId = itemId;
+        this.action = action;
+        this.relatedTo = relatedTo;
+        this.message = message;
+    }
 
     public String getItemId() {
         return itemId;
@@ -21,10 +33,28 @@ public class AdEvent {
         this.message = message;
     }
 
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getRelatedTo() {
+        return relatedTo;
+    }
+
+    public void setRelatedTo(String relatedTo) {
+        this.relatedTo = relatedTo;
+    }
+
     @Override
     public String toString() {
         return "AdEvent{" +
                 "itemId='" + itemId + '\'' +
+                ", action='" + action + '\'' +
+                ", relatedTo='" + relatedTo + '\'' +
                 ", message='" + message + '\'' +
                 '}';
     }
