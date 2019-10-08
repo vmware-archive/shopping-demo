6187f4c99837044568ebd67c70dae5a26b5d8014
Florent Biville <fbiville@pivotal.io>
2019-10-08 16:15:57 -0500

    Reinstate script

---
+++ run-ad-pipeline.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+function tear_down() {
+  riff streaming processor delete "ad" &>/dev/null || true
+  riff function delete "ad-recommender" &>/dev/null || true
+  riff streaming stream delete "cart-events" &>/dev/null || true
+  riff streaming stream delete "ad-events" &>/dev/null || true
+  riff streaming stream delete "user-ads" &>/dev/null || true
+}
+
+function set_up() {
+  riff streaming stream create "cart-events" \
+    --provider franz-kafka-provisioner \
+    --content-type 'application/json'
+
+  riff streaming stream create "ad-events" \
+    --provider franz-kafka-provisioner \
+    --content-type 'application/json'
+
+  riff streaming stream create "user-ads" \
+    --provider franz-kafka-provisioner \
+    --content-type 'application/json'
+
+  riff function create "ad-recommender" \
+    --git-repo https://github.com/sbawaska/shopping-demo.git \
+    --sub-path ad-recommender/ \
+    --tail
+
+  riff streaming processor create "ad" \
+    --function-ref ad-recommender \
+    --input "cart-events" \
+    --input "ad-events" \
+    --output "user-ads" \
+    --tail
+
+  echo "💡 about to expose the gateway to local port 8080"
+  echo "🚀️ open another terminal and start ingesting events️"
+  echo
+  kubectl -n riff-system port-forward "svc/riff-streaming-http-gateway" "8080:80"
+}
+
+function main() {
+  tear_down
+  set_up
+}
+
+main
