eedf07a695a2225c3cdece432da2038c977ec321
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-10-09 00:18:52 -0500

    define function as a bean

--- ad-recommender/src/main/java/io/projectriffdemo/shopping/AdRecommenderApplication.java
+++ ad-recommender/src/main/java/io/projectriffdemo/shopping/AdRecommenderApplication.java
@@ -3,6 +3,11 @@ package io.projectriffdemo.shopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 @SpringBootApplication
 public class AdRecommenderApplication {
@@ -12,7 +17,47 @@ public class AdRecommenderApplication {
     }
 
     @Bean
-    public AdRecommenderFunction adRecommenderFunction() {
-        return new AdRecommenderFunction();
+    public Function<Tuple2<Flux<CartEvent>, Flux<AdEvent>>, Flux<UserAd>> adRecommender() {
+        return events -> {
+            Flux<CartEvent> cartFlux = events.getT1().log("cart events");
+            Flux<AdEvent> adFlux = events.getT2().log("cart events");
+
+            return cartFlux
+                    .join(
+                            adFlux,
+                            just2(Flux.never()),
+                            just2(Flux.never()),
+                            this::computeRelevantAd
+                    )
+                    .log("maybe relevant ads")
+                    .filter(Optional::isPresent)
+                    .log("relevant ads")
+                    .map(Optional::get)
+                    .log("definitely relevant ads");
+        };
+    }
+
+    private Optional<UserAd> computeRelevantAd(CartEvent cartEvent, AdEvent adEvent) {
+        Optional<Ad> maybeAd = Optional.empty();
+        if (showMainItemMatchingAd(cartEvent, adEvent)) {
+            maybeAd = mainItemAd(adEvent);
+        }
+        return maybeAd.map(ad -> new UserAd(cartEvent.getUserId(), ad));
+    }
+
+    private boolean showMainItemMatchingAd(CartEvent userCartEvent, AdEvent adEvent) {
+        String action = userCartEvent.getAction();
+        String cartItemId = userCartEvent.getItemId();
+        return action.equals("add") && adEvent.getRelatedTo().equals(cartItemId) ||
+                action.equals("remove") && adEvent.getItemId().equals(cartItemId);
+    }
+
+    private Optional<Ad> mainItemAd(AdEvent adEvent) {
+        return Optional.of(new Ad(adEvent.getItemId(), true, adEvent.getMessage()));
+    }
+
+
+    private <T, R> Function<T, Flux<R>> just2(final Flux<R> publisher) {
+        return t1 -> publisher;
     }
 }
